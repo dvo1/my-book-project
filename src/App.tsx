723aceb7b70943c0a4ad44076d76f36941d94e6a
dvo1 <babyductss@gmail.com>
2024-10-26 09:15:52 +0100

    second commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Routes } from "react-router-dom";
+import HomePage from './home-page';
+import "./App.css";
+const App = () => {
+  return (
+    <>
+    <Routes>
+    <Route path="/" element={<HomePage />} />
+    </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Scan these files for Tailwind classes
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
